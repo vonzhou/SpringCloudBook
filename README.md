971c7f3f57f8a0b5b5a788987771ac9d04fbc68d
vonzhou <vonzhou@163.com>
2018-04-03 21:05:42 +0800

    fuck

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 
 
 
+
+|project|desc|
+|--|--|
+|spring-boot-hello|spring boot入门|
+|eureka-server <br> hello-service-0|服务注册中心，注册服务|
+|eureka-server-ha|服务注册中心高可用|
+|eureka-server <br> hello-service-0<br>ribbon-consumer|消费服务|
+|||
+
+
+
+
--- ribbon-consumer/src/main/java/com/didispace/web/ConsumerController.java
+++ ribbon-consumer/src/main/java/com/didispace/web/ConsumerController.java
@@ -4,12 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
     HelloService helloService;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/ribbon-consumer1", method = RequestMethod.GET)
+    public String helloConsumer1() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
 
     @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
     public String helloConsumer() {
