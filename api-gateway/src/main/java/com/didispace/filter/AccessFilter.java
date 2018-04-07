4699d86c2b98cc669adad4eddc1a2ccf31405f71
vonzhou <vonzhou@163.com>
2018-04-07 22:05:52 +0800

    Zuul

--- api-gateway/src/main/java/com/didispace/filter/AccessFilter.java
+++ api-gateway/src/main/java/com/didispace/filter/AccessFilter.java
@@ -4,10 +4,11 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class AccessFilter extends ZuulFilter  {
+public class AccessFilter extends ZuulFilter {
 
     private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
 
@@ -34,10 +35,10 @@ public class AccessFilter extends ZuulFilter  {
         log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
 
         Object accessToken = request.getParameter("accessToken");
-        if(accessToken == null) {
+        if (accessToken == null) {
             log.warn("access token is empty");
             ctx.setSendZuulResponse(false);
-            ctx.setResponseStatusCode(401);
+            ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
             return null;
         }
         log.info("access token ok");
