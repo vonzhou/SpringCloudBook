59a873d73c7c05a09898e00462561cd0ab79382d
vonzhou <vonzhou@163.com>
2018-04-08 17:16:44 +0800

    修改配置

--- README.md
+++ README.md
@@ -10,7 +10,8 @@
 |eureka-server <br> hello-service-0|服务注册中心，注册服务|
 |eureka-server-ha|服务注册中心高可用|
 |eureka-server <br> hello-service-0<br>ribbon-consumer|消费服务|
-|||
+|config-server <br> <config-client>|配置中心|
+|eureka-server <br> config-server-eureka <br> <config-client-eureka>|服务化配置中心|
 
 
 
--- config-client/src/main/java/com/didispace/web/TestController.java
+++ config-client/src/main/java/com/didispace/web/TestController.java
@@ -1,7 +1,9 @@
 package com.didispace.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +14,17 @@ public class TestController {
     @Value("${from}")
     private String from;
 
+    @Autowired
+    private Environment environment;
+
     @RequestMapping("/from")
     public String from() {
         return this.from;
+    }
+
+    @RequestMapping("/from2")
+    public String from2() {
+        return environment.getProperty("from", "from undefined");
     }
 
     public void setFrom(String from) {
